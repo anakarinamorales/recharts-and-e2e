ad36926df4f7bbfb5684197eeec18d1450f04315
Ana Karina Morales Machado <akarinammachado@gmail.com>
2025-01-15 18:05:15 +0100

    Fixed menu behaviour when navigating to detail page.

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -3,15 +3,25 @@ import Link from 'next/link';
 import { useMetersContext } from '@/context/useMetersContext';
 
 import styles from './Menu.module.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 
 export default function Menu() {
   const { meters, queryError } = useMetersContext();
   const [ariaExpanded, setAriaExpanded] = useState(() => false);
+  const router = useRouter();
 
   function toggleMenu() {
     setAriaExpanded(!ariaExpanded);
   }
+
+  // The ariaExpanded state is maintained between renders because the parent component (PageLayout) hasn't changed.
+  // So, navigating between /meter/1 and /meter/2 will not reset the state related to the aria-expanded for the navbar.
+  // That's why I'm manually ensuring that the state is cleared once the url changes.
+  // https://nextjs.org/docs/pages/api-reference/functions/use-router#resetting-state-after-navigation
+  useEffect(() => {
+    setAriaExpanded(false);
+  }, [router.query.id]);
 
   return (
     <nav
@@ -38,7 +48,9 @@ export default function Menu() {
         {!queryError &&
           meters?.map((meter) => (
             <li key={meter.name + meter.id} className={styles.menuItem}>
-              <Link href={`/meter/${meter.id}`}>{meter.name}</Link>
+              <Link href={`/meter/${meter.id}`} prefetch>
+                {meter.name}
+              </Link>
             </li>
           ))}
       </ul>
